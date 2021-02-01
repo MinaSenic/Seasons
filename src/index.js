d45abc3330116aeb345904f6a54b41fdaa62a610
Nikola_MIna <nikola_mina@nikolakikis.local>
2021-02-01 22:20:21 +0100

    Conditional rendering & lifecycle methods

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // THIS IS THE ONLY TIME we do direct assignment to this.state
+        this.state = { lat: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // we called setState!
+this.setState({lat:position.coords.latitude});
+
+// WE DID NOT DO this.state.lat=position.coords.latitude
+            },
+            err => {
+                this.setState({errorMessage: err.message});
+            }
+        );
+    }
+
+// For the purposes of understanding Lifecycle Methods we are adding ComponentDidMount and ComponentDidUpdate methods
+// that will be automatically called at some point in time
+
+//Right above the Render method I'll define:
+
+componentDidMount () {
+console.log('My component was rendered to the screen!');
+}
+
+// Now I'll define a second Lifecycle method:
+componentDidUpdate () {
+console.log('My component was just updated - It rerendered!');
+}
+
+
+    render() {
+        //CASE 1 - NO LAT, DO HAVE ERROR MESSAGE 
+     if (this.state.errorMessage && !this.state.lat) {
+         return <div> Error: {this.state.errorMessage}</div>
+     }
+     // CASE 2 - NO ERROR MESSAGE, DO HAVE LAT
+     if (!this.state.errorMessage && this.state.lat) {
+         return <div>Latitude: {this.state.lat} </div>
+     }
+     // CASE 3 - NO LAT, NO ERROR MESSAGE, show loading
+     
+     return <div>Loading!!!</div>
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <App />,
+    document.querySelector('#root')
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
